ff8564ab1bbabf646924966f9b19ac9ddb239843
Ezhilvj <www.ezhilvj333@gmail.com>
2022-01-06 08:08:20 +0530

    new project into git

---
+++ src/org/test/Hello.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int i=18;
+		
+		if (i<=10) {
+			System.out.println("Eligible For Voting");
+		}
+		else {
+			System.out.println("Not Eligible For Voting ");
+		}
+	
+			
+			}
+			
+		}
+		
+	
+
